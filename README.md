3d51bd15787c4269d210c81d78491ef8eb3e3b90
nathandao <nathan@guynathan.com>
2020-05-05 13:39:06 +0300

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Test asset files in Contentful
+
+Copy .env.example to .env and fill up the values
+
+```bash
+cp .env.example .env
+```
+
+Install dependencies
+
+```bash
+npm i
+```
+
+Start
+
+```bash
+npm start
+```
+
+- /files: List all the published PDFDocuments content type
+- /preview-files: List all the published PDFDocuments content type
---
+++ index.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+
+const express = require("express");
+const contentful = require("contentful");
+const _ = require("lodash");
+const richTextTypes = require("@contentful/rich-text-types");
+const toHTMLString = require("@contentful/rich-text-html-renderer")
+  .documentToHtmlString;
+
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_TOKEN,
+});
+
+const previewClient = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+  host: "preview.contentful.com",
+});
+
+async function getArticleEntries() {
+  return publicArticles;
+}
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const pdfDocuments = await client.getEntries({
+    content_type: "pdfDocument",
+  });
+  const items = pdfDocuments.items.map((item) => ({
+    url: _.get(item, "fields.file.fields.file.url"),
+    title: _.get(item, "fields.title"),
+  }));
+  res.setHeader("Content-Type", "application/json");
+  res.send(items);
+});
+
+app.get("/files", async (req, res) => {
+  const pdfDocuments = await client.getEntries({
+    content_type: "pdfDocument",
+  });
+  const items = pdfDocuments.items.map((item) => ({
+    url: _.get(item, "fields.file.fields.file.url"),
+    title: _.get(item, "fields.title"),
+  }));
+  res.setHeader("Content-Type", "application/json");
+  res.send(items);
+});
+
+app.get("/preview-files", async (req, res) => {
+  const pdfDocuments = await previewClient.getEntries({
+    content_type: "pdfDocument",
+  });
+  const items = pdfDocuments.items.map((item) => ({
+    url: _.get(item, "fields.file.fields.file.url"),
+    title: _.get(item, "fields.title"),
+  }));
+  res.setHeader("Content-Type", "application/json");
+  res.send(items);
+});
+
+app.listen(port, () => {
+  console.log(`Server started at localhost: ${port}`);
+});
